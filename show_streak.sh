5cda939b51b9282bf29c0083118b8b0bdab7a5cf
Finn Ellis <relsqui@chiliahedron.com>
2017-02-13 06:39:18 -0800

    Add streak function (oops) and convert to dots. Addresses #4.

---
+++ show_streak.sh
@@ -0,0 +1,42 @@
+doily::get_streak() {
+    total="$(ls "${doily_dir}" | wc -l)"
+    if [[ "${total}" -eq 0 ]]; then
+        # No dailies, so no streak.
+        return
+    fi
+
+    dots=""
+    count=0
+    streak=0
+    streak_broken=false
+    first_filename="${doily_dir}/$(ls "${doily_dir}" | head -n 1)"
+    seen_all=false
+    while ! ${seen_all}; do
+        daily_filename="${doily_dir}/$(date +%F --date="${count} days ago")"
+        if [[ -e "${daily_filename}" ]]; then
+            dots="o${dots}"
+            if ! ${streak_broken}; then
+                (( streak += 1 ))
+            fi
+        else
+            dots=".${dots}"
+            streak_broken=true
+        fi
+        if [[ "${daily_filename}" == "${first_filename}" ]]; then
+            seen_all=true
+        fi
+        (( count += 1 ))
+    done
+
+    if [[ "$streak" -eq 1 ]]; then
+        if [[ "${total}" -gt "$streak" ]]; then
+            # Just came back after a gap.
+            echo "Streak: ${dots} Welcome back!"
+        else
+            # First post!
+            echo "Streak: ${dots} Welcome!"
+        fi
+    elif [[ "$streak" -gt 1 ]]; then
+        echo "Streak: ${dots} Nice."
+    fi
+}
