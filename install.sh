787bba8895d3814c67787e4b3db5741a9190fb96
Finn Ellis <relsqui@chiliahedron.com>
2017-03-03 01:54:04 -0800

    Add main logic of install script, pushing to test.

--- install.sh
+++ install.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
+# Doily install script.
+# (c) 2017 Finn Ellis, licensed under MIT.
+# https://github.com/relsqui/doily
+
+# Install a doily release from the Github repository, either systemwide
+# or for the current user; or, uninstall doily for the system or user.
+# Does not destroy user-specific configuration or data in any case.
+
+# Everything here except VERSION is just for development and testing.
 VERSION="install_test"
+BRANCH="install-script"
+TARGET="user"
+ACTION="install"
 
-tarball="doily-${VERSION}.tar"
-release_url="https://raw.githubusercontent.com/relsqui/doily/master/releases/${tarball}"
-old_pwd="${PWD}"
+if [[ "${TARGET}" == "user" ]]; then
+    binary_dir="$HOME/bin"
+    config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/doily"
+else
+    binary_dir="/usr/local/bin"
+    config_dir="/usr/local/etc/doily"
+fi
 
-fetch_release() {
-    # Retrieve the release tarball into a temp directory and cd to it.
+if [[ "${ACTION}" == "uninstall" ]]; then
+    rm -v "${binary_dir}/doily"
+    rm -vr "${config_dir}"
+    echo "Add a note here about user data not being removed, and how to."
+else
     tempdir="$(mktemp --tmpdir -dt doily-XXXXX)"
-    echo "curl -N \"${release_url}\""
-    curl -N "${release_url}" | tar -xvC "${tempdir}"
-    ls "${tempdir}"
-}
-
-systemwide_install() {
-    # Create the appropriate target directories and move install files.
-    mkdir -pv /usr/local/bin/
-    mkdir -pv /usr/local/lib/doily/plugins
-    mkdir -pv /usr/local/etc/doily/plugins
-    mv -v doily /usr/local/bin
-    mv -v default.conf /usr/local/etc/doily
-}
-
-cleanup() {
-    cd "${old_pwd}"
-    rmdir "${tempdir}"
-}
-
-main() {
-    fetch_release
-}
-
-main
+    release_url="https://raw.githubusercontent.com/relsqui/doily/${BRANCH}/releases/doily-${VERSION}.tar"
+    curl "${release_url}" | tar -vxC "${tempdir}"
+    mkdir -vp "${binary_dir}" "${config_dir}"
+    mv -v "${tempdir}/doily" "${binary_dir}"
+    if [[ "${TARGET}" == "user" ]]; then
+        # Don't clobber existing user configuration with the default.
+        mv -vn "${tempdir}/default.conf" "${config_dir}/doily.conf"
+        echo "Add a note here about editing your path."
+    else
+        # Check before clobbering the systemwide default.
+        mv -vi "${tempdir}/default.conf" "${config_dir}/default.conf"
+    fi
+    rm -vr "${tempdir}"
+fi
