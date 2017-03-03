7a21eb50d04746bf1a31fcf96f9c5309baa16bbb
Finn Ellis <relsqui@chiliahedron.com>
2017-03-03 11:32:05 -0800

    Adhere release script to style guide.

--- make_release.sh
+++ make_release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -z "${1}" ]]; then
+if [[ -z "$1" ]]; then
     echo "Please provide a release name."
     exit 1
 fi
-new_release="releases/doily-${1}.tar.gz"
+new_release="releases/doily-$1.tar.gz"
 tar -czf "${new_release}" doily default.conf
 ls -l "${new_release}"
