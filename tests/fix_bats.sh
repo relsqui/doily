4135f86c232caee24bf62c9b32cc40c52a5b04ab
Finn Ellis <relsqui@chiliahedron.com>
2017-03-18 03:27:37 -0700

    Correct location of Bats in Travis.

--- tests/fix_bats.sh
+++ tests/fix_bats.sh
@@ -1,2 +1,2 @@
 # This fixes a known bug in Bats (https://github.com/sstephenson/bats/issues/140#issuecomment-206756745)
-sudo sed -i "20s/.*/  _count=\$(bats-exec-test -c \"\$filename\"); : \"\$(( count+=_count ))\"/" /usr/lib/bats/bats-exec-suite
+sudo sed -i "20s/.*/  _count=\$(bats-exec-test -c \"\$filename\"); : \"\$(( count+=_count ))\"/" /usr/local/lib/bats/bats-exec-suite
