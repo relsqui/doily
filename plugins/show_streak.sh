fbb4b6b391bcf66ebfbf49f7e6fb7a00a48f6f39
Finn Ellis <relsqui@chiliahedron.com>
2017-03-30 03:41:15 -0700

    Implement pre- and post-write plugins! #23

--- plugins/show_streak.sh
+++ plugins/show_streak.sh
@@ -1,5 +1,44 @@
-doily::get_streak() {
-    total="$(ls "${doily_dir}" | wc -l)"
+################################################################################
+# Doily plugin to show streak of daily writing.
+# https://github.com/relsqui/doily
+#
+# (c) 2017 Finn Ellis.
+# You are free to use, copy, modify, etc. this by the terms of the MIT license.
+# See included LICENSE.txt for details.
+#
+# After writing, shows a series of . and o indicating how many days in a row
+# you've written something (that is, days for which there's a dated file in
+# your dailies directory).
+#
+# This is also the test file for the new plugin system! Yay!
+################################################################################
+
+DOILY_CONF="${XDG_CONFIG_HOME:-$HOME/.config}/doily/doily.conf"
+PLUGIN_CONF="${XDG_CONFIG_HOME:-$HOME/.config}/doily/plugins/show_streak.conf"
+DAILIES="${XDG_DATA_HOME:-$HOME/.local/share}/doily/dailies"
+
+custom_command() {
+    # Not in use. Don't delete this!
+    :
+}
+
+pre_write() {
+    # Not in use. Don't delete this!
+    :
+}
+
+post_write() {
+    ############################################################################
+    # Calculate the streak display and echo it.
+    #
+    # Globals:
+    #    - DAILIES
+    # Args:
+    #    - None.
+    # Returns:
+    #    - None.
+    ############################################################################
+    total="$(ls "${DAILIES}" | wc -l)"
     if [[ "${total}" -eq 0 ]]; then
         # No dailies, so no streak.
         return
@@ -9,10 +48,10 @@ doily::get_streak() {
     count=0
     streak=0
     streak_broken=false
-    first_filename="${doily_dir}/$(ls "${doily_dir}" | head -n 1)"
+    first_filename="${DAILIES}/$(ls "${DAILIES}" | head -n 1)"
     seen_all=false
     while ! ${seen_all}; do
-        daily_filename="${doily_dir}/$(date +%F --date="${count} days ago")"
+        daily_filename="${DAILIES}/$(date +%F --date="${count} days ago")"
         if [[ -e "${daily_filename}" ]]; then
             dots="o${dots}"
             if ! ${streak_broken}; then
@@ -43,3 +82,42 @@ doily::get_streak() {
         echo "Streak: ${dots}"
     fi
 }
+
+main() {
+    ############################################################################
+    # Parses arguments and calls the appropriate other function. Plugin authors
+    # DO NOT NEED TO EDIT THIS, just edit the functions for whichever hooks
+    # your plugin should respond to.
+    #
+    # Globals:
+    #    - DOILY_CONF
+    #    - PLUGIN_CONF
+    # Config Vars:
+    #    - DAILIES
+    # Args:
+    #    - The name of a hook ("pre_write" or "post_write"), or "command"
+    #      followed by the command name and any arguments it requires.
+    # Returns:
+    #    - None.
+    ############################################################################
+    source "${DOILY_CONF}"
+    if [[ -e "${PLUGIN_CONF}" ]]; then
+        source "${PLUGIN_CONF}"
+    fi
+    case "$1" in
+        pre_write)
+            shift
+            pre_write
+            ;;
+        post_write)
+            shift
+            post_write
+            ;;
+        command)
+            shift
+            custom_command "$@"
+            ;;
+    esac
+}
+
+main "$@"
